06f26f6f172a34c77d1555b2ffc8aaf7568b481e
cooke09 <pozdeev199@mail.ru>
2023-04-25 12:57:16 +0300

    Фронт странички преподавателей. К модели User добавлено изображение профиля

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
-from sqlalchemy import Column as _, String, Boolean, Integer, ForeignKey, \
-    PrimaryKeyConstraint, UniqueConstraint
+import sqlalchemy as sa
 from sqlalchemy.orm import relationship, backref
 
 from src.database import BaseModel
@@ -19,20 +18,22 @@ roles_as_dict = {i.name: i.value for i in RolesType}
 
 class User(BaseModel):
     __tablename__ = 'users'
-    first_name = _(String(50), nullable=True)
-    last_name = _(String(99), nullable=True)
-    username = _(String(50), unique=True, nullable=True)
-    email = _(String(99), unique=True, nullable=False)
-    password = _(String, nullable=False)
-    is_active = _(Boolean, default=False)
-    role = _(ChoiceType(roles_as_dict), nullable=False,
-             default=RolesType.student.value)
+    first_name = sa.Column(sa.String(50), nullable=True)
+    last_name = sa.Column(sa.String(99), nullable=True)
+    username = sa.Column(sa.String(50), unique=True, nullable=True)
+    email = sa.Column(sa.String(99), unique=True, nullable=False)
+    password = sa.Column(sa.String, nullable=False)
+    is_active = sa.Column(sa.Boolean, default=False)
+    role = sa.Column(ChoiceType(roles_as_dict), nullable=False,
+                     default=RolesType.student.value)
     verify_code = relationship('Verification', back_populates='user_to_verify')
+    user_image = relationship(
+        'UserProfileImage', back_populates='user', uselist=False)
 
     __table_args__ = (
-        PrimaryKeyConstraint('id', name='user_pk'),
-        UniqueConstraint('username'),
-        UniqueConstraint('email'),
+        sa.PrimaryKeyConstraint('id', name='user_pk'),
+        sa.UniqueConstraint('username'),
+        sa.UniqueConstraint('email'),
     )
 
 
@@ -48,7 +49,16 @@ staf_as_dict = {i.name: i.value for i in StaffType}
 
 class Staff(BaseModel):
     __tablename__ = 'staffs'
-    staff_role = _(ChoiceType(staf_as_dict), nullable=False,
-                   default=StaffType.admin.value)
-    user_id = _(Integer, ForeignKey('users.id'))
+    staff_role = sa.Column(ChoiceType(staf_as_dict), nullable=False,
+                           default=StaffType.admin.value)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey('users.id'))
     user = relationship(User, backref=backref('staff', uselist=False))
+
+
+class UserProfileImage(BaseModel):
+    __tablename__ = 'users_profiles_images'
+    photo_blob = sa.Column(sa.LargeBinary, nullable=False)
+    photo_type = sa.Column(sa.String, nullable=True)
+    user_id = sa.Column(
+        sa.Integer, sa.ForeignKey('users.id'))
+    user = relationship('User', back_populates='user_image')
--- frontend/src/pages/TeachersPage/TeachersPage.jsx
+++ frontend/src/pages/TeachersPage/TeachersPage.jsx
@@ -1,7 +1,78 @@
-import React from 'react'
+import React from "react";
+import Input from "../../components/UI/BaseInput/Input";
+import cls from "./TeachersPage.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import ButtonAsLink from "../../components/UI/ButtonAsLink/ButtonAsLink";
+import CourseBase from "../../img/course_base.png";
+
+const TeacherItem = ({ teacher_data }) => {
+  return (
+    <>
+      <div className={cls.box}>
+        <div className={cls.teacher}>
+          <img src={CourseBase} alt="about_pic" />
+          <div>
+            <h3>Teacher name</h3>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit harum quos nostrum.
+            </span>
+          </div>
+        </div>
+        <p>
+          Course <span>Amount of courses</span>
+        </p>
+        <p>
+          Total comments <span>Amount of comments</span>
+        </p>
+        <p>
+          Total rating courses <span>Total rating</span>
+        </p>
+        <ButtonAsLink
+          href="/teachers/1/profile"
+          button_type="inline"
+          btn_action="click"
+        >
+          Перейти на страничку
+        </ButtonAsLink>
+      </div>
+    </>
+  );
+};
 
 export default function TeachersPage() {
   return (
-    <div>TeachersPage</div>
-  )
+    <section>
+      <h1 className="section_header">преподаватели</h1>
+      <div className={cls.search_teacher}>
+        <Input required placeholder="Поиск преподавателя" />
+        <button>
+          <FontAwesomeIcon icon={faSearch} className={cls.side_icon} />
+        </button>
+      </div>
+
+      <div className={cls.box_container}>
+        <div className={[cls.box, cls.offer].join(" ")}>
+          <h3>Стать преподавателем</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia, eum.
+            Laborum saepe itaque omnis beatae similique magnam delectus tenetur.
+            Ipsa eligendi explicabo doloremque iure saepe corporis recusandae
+            architecto maxime neque?
+          </p>
+          <ButtonAsLink
+            href="/register"
+            button_type="inline"
+            btn_action="click"
+          >
+            зарегестрироваться
+          </ButtonAsLink>
+        </div>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </div>
+    </section>
+  );
 }
